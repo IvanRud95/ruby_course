79b65ea7421b5a0a47f0e12f35bebbfcaa8bc540
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2018-12-11 07:27:06 +0300

    Create movies2.rb

---
+++ task2/movies2.rb
@@ -0,0 +1,7 @@
+File.open('movies.txt').each do |line|
+  puts line.split(/\|(?=[\w])/)
+end
+ax.select{|x| x if x[1].include? 'Max'}.each do |y|
+ puts y[1]
+ puts '*' * ((y[7].to_f - 8)*10).to_i
+end
