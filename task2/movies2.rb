f791816cdb469a42ad40daa9258dd06ca587bae5
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2018-12-11 07:28:50 +0300

    Update movies2.rb

--- task2/movies2.rb
+++ task2/movies2.rb
@@ -1,6 +1,20 @@
+movies = {
+    'Matrix'  => 'Matrix is a good movie'
+    'Titanic' => 'Titanic is - a bad movie' }
+
+movie_name = ARGV[0]
+
+  if movies.key? movie_name
+    puts movies[movie_name]
+  else
+    puts "Haven't seen '#{movie_name}' yet"
+  end
+
+
 File.open('movies.txt').each do |line|
   puts line.split(/\|(?=[\w])/)
 end
+
 ax.select{|x| x if x[1].include? 'Max'}.each do |y|
  puts y[1]
  puts '*' * ((y[7].to_f - 8)*10).to_i
