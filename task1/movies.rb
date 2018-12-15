b44e3ebe1a289e8ddb93f5f4cdc0b1ac75a88ef6
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2018-12-15 13:59:42 +0300

    Update movies.rb

--- task1/movies.rb
+++ task1/movies.rb
@@ -1,10 +1,29 @@
-KEYS = [:link, :title, :year, :country, :date, :jenre, :duration, :rating, :author, :actors].freeze
+KEYS = [:link, :name, :year, :country, :date, :genre, :duration, :rating, :author, :actors].freeze
 
-ax=[]
-File.open('movies.txt').each do |line|
-  ax<<line.split(/\|(?=[\w])/)
+def show_films(sorted)
+  sorted.each_with_index do |movie, index|
+    puts "#{index + 1}.#{movie[:name]}: (#{movie[:date]}\; #{movie[:genre]}) - #{movie[:duration]}"
+  end
 end
-ax.select{|x| x if x[1].include? 'Max'}.each do |y|
-  puts y[1]
-  puts '*' * ((y[7].to_f - 8)*10).to_i
-end
+
+file_path = ARGV[0] || 'movies.txt'
+
+f = File.open(file_path)
+films = f.map {|movie| KEYS.zip(movie.strip.split('|')).to_h}
+
+sorted = films.sort_by {|movie| movie[:duration].split(' ')[0].to_i}.last(5)
+puts "Films by duration: "
+show_films(sorted)
+
+sorted = films.sort_by {|movie| movie[:genre]}
+             .select {|movie| movie[:genre].split(',').include?('Comedy')}
+             .sort_by {|movie| movie[:date]}.first(10)
+puts "Comedies:"
+show_films(sorted)
+
+puts "Authors:"
+sorted = films.map {|movie| movie[:author]}.sort_by {|name| name.split(' ').last}
+puts sorted
+
+puts "Number of films out of USA:"
+puts films.count { 
