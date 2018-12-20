0673120bb19373d5ddaad69ab0c02c34b5923af6
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2018-12-20 11:43:28 +0300

    Update movies.rb

--- task1/movies.rb
+++ task1/movies.rb
@@ -1,5 +1,7 @@
 KEYS = [:link, :name, :year, :country, :date, :genre, :duration, :rating, :author, :actors].freeze
 
+MONTH_NAMES = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December].freeze
+
 movies = CSV.foreach(FILE_NAME, col_sep: '|', headers: KEYS).map { |movie_arr| build_movie(movie_arr) }
 
 def show_films(sorted)
@@ -9,6 +11,22 @@ def show_films(sorted)
 end
 
 file_path = ARGV[0] || 'movies.txt'
+
+def build_movie(data)
+  data = data.to_h
+
+  OpenStruct.new(
+    data.merge({
+      year: data[:year].to_i,
+      duration: data[:duration].to_i,
+      rating: data[:rating].to_f,
+      genres: data[:genres].split(','),
+      starring: data[:starring].split(','),
+      month: get_month(data[:date]),
+      date: parse_date(data[:date])
+    })
+  )
+end
 
 f = File.open(file_path)
 films = f.map {|movie| KEYS.zip(movie.strip.split('|')).to_h}
