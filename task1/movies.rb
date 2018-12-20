cf7200c27594297ed0dc6071b464217f4ffb76b2
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2018-12-20 11:58:05 +0300

    Update movies.rb

--- task1/movies.rb
+++ task1/movies.rb
@@ -1,8 +1,9 @@
+require 'csv'
+require 'ostruct'
+
 KEYS = [:link, :name, :year, :country, :date, :genre, :duration, :rating, :author, :actors].freeze
 
 MONTH_NAMES = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December].freeze
-
-movies = CSV.foreach(FILE_NAME, col_sep: '|', headers: KEYS).map { |movie_arr| build_movie(movie_arr) }
 
 def show_films(sorted)
   sorted.each_with_index do |movie, index|
@@ -12,24 +13,14 @@ end
 
 file_path = ARGV[0] || 'movies.txt'
 
-def build_movie(data)
-  data = data.to_h
-
-  OpenStruct.new(
-    data.merge({
-      year: data[:year].to_i,
-      duration: data[:duration].to_i,
-      rating: data[:rating].to_f,
-      genres: data[:genres].split(','),
-      starring: data[:starring].split(','),
-      month: get_month(data[:date]),
-      date: parse_date(data[:date])
-    })
-  )
+unless File.exist?(file_path)
+  puts "file #{file_path} dont exist"
+  exit 1
 end
 
-f = File.open(file_path)
-films = f.map {|movie| KEYS.zip(movie.strip.split('|')).to_h}
+f = CSV.open(file_path, col_sep: '|', encoding: 'UTF-8')
+films = f.map { |row| OpenStruct.new(KEYS.zip(row).to_h) }
+
 
 sorted = films.sort_by {|movie| movie[:duration].split(' ')[0].to_i}.last(5)
 puts "Films by duration: "
@@ -46,5 +37,5 @@ puts "Number of films without USA:"
 puts films.count { |m| m[:country] != 'USA' }
 
 puts "Authors:"
-sorted = films.map {|movie| movie[:author]}.sort_by {|name| name.split(' ').last}
-puts sorte
+sorted = films.map {|movie| movie[:author]}.sort_by {|name| name.split(' ').last}.uniq
+puts sorted
