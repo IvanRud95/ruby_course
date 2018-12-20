3c17086581d820e46a977a8c0d559e61b959ef49
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2018-12-20 12:07:35 +0300

    Update movies.rb

--- task1/movies.rb
+++ task1/movies.rb
@@ -1,9 +1,10 @@
 require 'csv'
 require 'ostruct'
+require 'date'
 
 KEYS = [:link, :name, :year, :country, :date, :genre, :duration, :rating, :author, :actors].freeze
 
-MONTH_NAMES = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December].freeze
+MONTH = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December].freeze
 
 def show_films(sorted)
   sorted.each_with_index do |movie, index|
@@ -19,22 +20,34 @@ unless File.exist?(file_path)
 end
 
 f = CSV.open(file_path, col_sep: '|', encoding: 'UTF-8')
-films = f.map { |row| OpenStruct.new(KEYS.zip(row).to_h) }
+films = f.map {|row| OpenStruct.new(KEYS.zip(row).to_h)}
 
+puts "_______"
+puts "Films by month : "
+sorted = films.select {|film| film.date.size == 10}.map {|film| Date.strptime(film.date).mon}
+films_by_month = sorted.each_with_object(Hash.new(0)) {|month, hsh| hsh[month] += 1}.sort.to_h
+films_by_month.each {|month, movies| puts "#{Date::MONTHNAMES[month]} - #{films_by_month[month]}"}
+puts "_______"
+
+sorted = films.sort_by {|movie| movie[:month]}.last(5)
+show_films(sorted)
+puts "_______"
 
 sorted = films.sort_by {|movie| movie[:duration].split(' ')[0].to_i}.last(5)
 puts "Films by duration: "
 show_films(sorted)
+puts "_______"
 
 sorted = films.sort_by {|movie| movie[:genre]}
              .select {|movie| movie[:genre].split(',').include?('Comedy')}
              .sort_by {|movie| movie[:date]}.first(10)
 puts "Comedies:"
 show_films(sorted)
-
+puts "_______"
 
 puts "Number of films without USA:"
-puts films.count { |m| m[:country] != 'USA' }
+puts films.count {|m| m[:country] != 'USA'}
+puts "_______"
 
 puts "Authors:"
 sorted = films.map {|movie| movie[:author]}.sort_by {|name| name.split(' ').last}.uniq
