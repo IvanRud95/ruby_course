2e0df225c3c16ae0976261bcadec14fb680bb435
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2019-01-02 16:08:28 +0300

    Update demo.rb

--- task1/demo.rb
+++ task1/demo.rb
@@ -10,28 +10,31 @@ end
 
 movies = MovieCollection.new(file_path)
 
-puts "Check one movie: "
+puts "First movie: "
 puts movies.all[0].to_s
+puts "________________ "
 
-puts "\nCheck first movie actors:"
+puts "first movie actors:"
 puts movies.all.first.actors
+puts "________________ "
 
-puts "\nCheck first movie is Comedy:"
+puts "Check first movie is Comedy:"
 puts movies.all.first.has_genre?('Comedy')
+puts "________________ "
 
-puts "\nCheck first movie is Drama:"
+puts "Check first movie is Drama:"
 puts movies.all.first.has_genre?('Drama')
+puts "________________ "
 
-puts "\nCheck all movies (first 5):"
-puts movies.all.first(5)
-
-puts "\nCheck movies sort (by date first 5 ):"
+puts "Check movies sort (by date first 5 ):"
 puts movies.movie_sort(:date).first(5)
+puts "________________ "
 
-puts "\nCheck movies filter (by genre first 5):"
+puts "Check movies filter (by genre first 5):"
 puts movies.filter(:genre, 'Comedy').first(5)
+puts "________________ "
 
-puts "\nCheck movies statistic (by author):"
+puts "Check movies statistic (by author):"
 movies = movies.stats(:author)
 movies.each do |author, movie_counter|
   puts "#{author} -- #{movies[author]}"
