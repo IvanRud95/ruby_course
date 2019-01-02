3be8f8ff9a9e7ae9ab8aae397e7880516ebea311
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2019-01-02 15:57:00 +0300

    Create movie.rb

---
+++ task1/movie.rb
@@ -0,0 +1,32 @@
+class Movie
+  attr_accessor :link,
+              :name,
+              :year,
+              :country,
+              :date,
+              :genre,
+              :duration,
+              :rating,
+              :author,
+              :actors
+  def initialize(movie = {})
+    @link = movie[:link]
+    @name = movie[:name]
+    @year = movie[:year]
+    @country = movie[:country]
+    @date = movie[:date]
+    @genre = movie[:genre]
+    @duration = movie[:duration]
+    @rating = movie[:rating]
+    @author = movie[:author]
+    @actors = movie[:actors]
+  end
+
+  def has_genre?(genre)
+    @genre.include?(genre) ? "yes it's #{genre}" : "no it's not #{genre}"
+  end
+
+  def to_s
+    "#{@name}: (#{@date}\; #{@genre}) - #{@duration} - #{@author}"
+  end
+end
