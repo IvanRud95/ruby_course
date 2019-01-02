c1ae60112bbd691cec99b863adb07880e26dc6f8
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2019-01-02 15:57:24 +0300

    Create MovieCollection.rb

---
+++ task1/MovieCollection.rb
@@ -0,0 +1,27 @@
+require_relative 'movie.rb'
+
+class MovieCollection
+  KEYS = [:link, :name, :year, :country, :date, :genre, :duration, :rating, :author, :actors].freeze
+  def initialize(file_path)
+    f = File.open(file_path, 'r:UTF-8')
+    @films =  f.map {  |film| Movie.new(KEYS.zip(film.strip.split('|')).to_h) }
+    f.close
+  end
+
+  def all
+    @films
+  end
+
+  def movie_sort(movie_field)
+    @films.sort_by { |film| film.send(movie_field) }
+  end
+
+  def filter(movie_field, value)
+    @films.select { |film| film.send(movie_field).include?(value) }
+  end
+
+  def stats(movie_field)
+    films_authors = movie_sort(movie_field).map { |film| film.send(movie_field) }
+    films_authors.each_with_object(Hash.new(0)) { |movie_field, hsh| hsh[movie_field] += 1 }.to_h
+  end
+end
