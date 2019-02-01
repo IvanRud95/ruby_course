874c8c671eea46f92a9936d5741f8a661b41327e
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2019-02-01 18:55:00 +0300

    Update demo.rb

--- task1/demo.rb
+++ task1/demo.rb
@@ -1,16 +1,19 @@
 require_relative 'movie_collection'
 require_relative 'movie'
 
-
 movies = MovieCollection.new
 
+
 puts movies.all
+puts "________________ "
 
 puts movies.all.first.actors
 puts movies.all.first(5).map(&:genre)
+puts "________________ "
 
 puts movies.sort_by(:genre)
 puts movies.sort_by(:year)
+puts "________________ "
 
 begin
    puts movies.all.first.has_genre?('Comedy')
@@ -19,12 +22,14 @@ begin
  rescue Exception => error
    puts error
  end
+puts "________________ "
 
  puts movies.filter(genre: 'Comedy', year: 2010)
  puts movies.filter(year: 2014..2015)
  puts movies.filter(year: 2010)
  puts movies.filter(director: /Coppola/)
  puts movies.filter(actors: /Hanks/)
+puts "________________ "
 
  puts movies.stats(:year).map { |m| m.flatten.join(' -> ') }
  puts movies.stats(:genre).map { |m| m.flatten.join(' -> ') }
