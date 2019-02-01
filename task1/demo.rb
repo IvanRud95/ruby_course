24635af58bdc92c3b48395c508f16c24ead44a6e
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2019-02-01 15:43:29 +0300

    Update demo.rb

--- task1/demo.rb
+++ task1/demo.rb
@@ -1,41 +1,34 @@
-require_relative 'movie'
-require_relative 'movie_collection'
-
-file_path = ARGV[0] || 'movies.txt'
-
-unless File.exist?(file_path)
-  puts "No such file #{file_path}"
-  exit 1
+movies = MovieCollection.new('movies.txt')
+%i(link title year month date country genres duration rating producer actors).each do |field|
+  puts movies.sort_by(field).first(5)
+  puts
 end
 
-movies = MovieCollection.new(file_path)
 
-puts "First movie: "
-puts movies.all[0].to_s
-puts "________________ "
+[
+  { genres: 'Comedy' }, { country: 'USA' }, { country: 'Russia' }, { title: /Terminator/i },
+  { year: 2000 }, { producer: 'Robert Zemeckis' }, { actors: 'Morgan Freeman' }, { actors: /Morgan/i },
+  { year: 2001..2008 }, { title: /Terminator/i, year: 1980..1990 }
+].each do |facet|
+  puts facet
+  puts movies.filter(facet).first(5).inspect
+  puts
+end
 
-puts "first movie actors:"
-puts movies.all.first.actors
-puts "________________ "
+puts '*** Stats ***'
+%i(month year country producer actors genres).each do |field|
+  puts movies.stats(field).inspect
+  puts
+end
 
-puts "Check first movie is Comedy:"
-puts movies.all.first.has_genre?('Comedy')
-puts "________________ "
+movie = movies.all.first
+puts movie.genres.inspect
+puts movie.has_genre?('Crime')
+puts movie.has_genre?('Comedy')
 
-puts "Check first movie is Drama:"
-puts movies.all.first.has_genre?('Drama')
-puts "________________ "
-
-puts "Check movies sort (by date first 5 ):"
-puts movies.movie_sort(:date).first(5)
-puts "________________ "
-
-puts "Check movies filter (by genre first 5):"
-puts movies.filter(:genre, 'Comedy').first(5)
-puts "________________ "
-
-puts "Check movies statistic (by author):"
-movies = movies.stats(:author)
-movies.each do |author, movie_counter|
-  puts "#{author} -- #{movies[author]}"
+puts movies.genres.inspect
+begin
+  movie.has_genre?('Tragedy')
+rescue Movie::GenreNotFoundError => e
+  puts e
 end
