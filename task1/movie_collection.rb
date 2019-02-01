c25cd82ec4b72d7cf47f209f71b166bd0ce61a4a
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2019-02-01 16:01:34 +0300

    Update movie_collection.rb

--- task1/movie_collection.rb
+++ task1/movie_collection.rb
@@ -1,13 +1,18 @@
+require 'csv'
+require 'date'
 require_relative 'movie.rb'
 
 class MovieCollection
   KEYS = [:link, :name, :year, :country, :date, :genre, :duration, :rating, :author, :actors].freeze
-  
+
   def initialize(file_path)
     f = File.open(file_path, 'r:UTF-8')
     @films =  f.map {  |film| Movie.new(KEYS.zip(film.strip.split('|')).to_h) }
-    f.close
     @genres = @films.flat_map(&:genres).uniq
+  end
+
+  def each(&block)
+    @films.each(&block)
   end
 
   def all
@@ -15,20 +20,28 @@ class MovieCollection
   end
 
   def movie_sort(movie_field)
-   @sort_by(&movie_field)
+    @sort_by(movie_field)
   end
+
 
   def filter(filters)
     filters.reduce(@films) { |result, (key, value)|
-    result.select {|films| movie.match?(key, value) } }
+      result.select {|films| films.match?(key, value) } }
   end
 
   def stats(movie_field)
     films_authors = movie_sort(movie_field).map { |film| film.send(movie_field) }
     films_authors.each_with_object(Hash.new(0)) { |movie_field, hsh| hsh[movie_field] += 1 }.to_h
   end
-  
-  def genre_exists?(genre)
-     @genres.include?(genre)
+
+  def print_stats(movie_field)
+    stats(movie_field).sort.each do |field_name, count|
+      puts "#{field_name}: #{count}"
+    end
   end
-end
+
+  def genre_exists?(genre)
+    @genres.include?(genre)
+  end
+
+  end
