31e547240e9c002c0f45f9d6289bf5e81314d641
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2019-02-04 18:08:38 +0300

    Update movie_collection_spec.rb

--- task1/spec/movie_collection_spec.rb
+++ task1/spec/movie_collection_spec.rb
@@ -1,65 +1,134 @@
-require 'lib/movie'
-require 'lib/movie_collection'
+require 'rspec'
+require 'rspec/its'
+require 'csv'
+require_relative '../lib/moviecollection'
+require_relative '../lib/movie'
 
 
-module IMDB
-  describe MovieCollection do
-    let(:movies) { described_class.new('movies.txt') }
+describe Imbd::MovieCollection do
 
-    describe '#all' do
-      subject { movies.all }
-      it { is_expected.to be_an(Array).and have_attributes(count: 250) }
-    end
+    subject(:collection) { described_class.new("lib/movies.txt") }
 
-    describe '#by_period' do
-      subject { movies.by_period(period) }
+    describe '#sort_by' do
 
-      context 'with empty period' do
-        let(:period) { nil }
-        it { is_expected.to have_attributes(count: 250) }
+      subject { collection.sort_by(criteria) }
+
+      context 'when filed not exist' do
+        let(:criteria) { :director1 }
+        it { expect { subject }.to raise_error(Imbd::MovieCollection::ParametrNotExist) }
       end
 
-    
+      Imbd::MovieCollection::KEYS.each do |field|
+        context "when #{field}" do
+          let(:criteria) { field }
+          its(:count) { should eq 250 }
+          it { is_expected.to be_an(Array).and all( be_an(Imbd::Movie) ) }
+          it { is_expected.to be_sorted_by(field) }
+        end
+      end
+
+    end
+
+    describe '#stats' do
+
+      subject { collection.stats(criteria) }
+
+      context 'when filed not exist' do
+        let(:criteria) { :director1 }
+        it { expect { subject }.to raise_error(Imbd::MovieCollection::ParametrNotExist) }
+      end
+
+      shared_examples "stats" do
+        it { is_expected.to be_an(Hash) }
+        its(:values) { are_expected.to all be_a(Fixnum) }
+      end
+
+      Imbd::MovieCollection::KEYS.each do |field|
+        context "when #{field}" do
+          let(:criteria) { field }
+          it_should_behave_like 'stats'
+        end
+      end
+
+    end
+
     describe '#filter' do
-      subject { movies.filter(facets) }
 
-      context 'empty facet' do
-        let(:facets) { {} }
-        it { is_expected.to eq(movies.all) }
+      subject { collection.filter(criteria) }
+
+      context 'when filed not exist' do
+        let(:criteria) { { director1: "James Cameron" } }
+        it { expect { subject }.to raise_error(Imbd::MovieCollection::ParametrNotExist) }
       end
 
-      context 'by period' do
-        let(:period) { :ancient }
-        let(:facets) { { period: period } }
-        it { expect(subject.map(&:period)).to all(be period) }
+      context 'when director' do
+        let(:criteria) { { director: "James Cameron" } }
+        it { is_expected.to all have_attributes(director: 'James Cameron') }
       end
 
-      context 'several filters' do
-        let(:facets) { { genres: 'Comedy', period: :classic } }
-        it { expect(subject.map(&:period)).to all(be :classic) }
-        it { expect(subject.map(&:genres)).to all(include('Comedy')) }
+      context 'when year' do
+        let(:criteria) { { year: 1940..2000 } }
+        it { is_expected.to all have_attributes(year: 1940..2000) }
       end
 
-      context 'several genres' do
-        let(:facets) { { genres: ['Comedy', 'Adventure'] } }
-        it { expect(subject.map(&:genres)).to all(include('Comedy').or include('Adventure')) }
+      context 'when title' do
+        let(:criteria) { { title: /ermi/i } }
+        it { is_expected.to all have_attributes(title: /ermi/i) }
       end
 
-      context 'several genres' do
-        let(:facets) { { genres: ['Drama', 'Horror'] } }
-        it { expect(subject.map(&:genres)).to all(include('Drama').or include('Horror')) }
+      context 'when country' do
+        let(:criteria) { { country: 'USA' } }
+        it { is_expected.to all have_attributes(country: 'USA') }
       end
+
+      context 'when date' do
+        let(:criteria) { { date: 1984 } }
+        it { is_expected.to all have_attributes(date: 1984) }
+      end
+
+      context 'when genre' do
+        let(:criteria) { { genre: "Action" } }
+        it { is_expected.to all have_attributes(genre: include("Action")) }
+      end
+
+      context 'when duration' do
+        let(:criteria) { { duration: /\d{3}/ } }
+        it { is_expected.to all have_attributes(duration: /\d{3}/) }
+      end
+
+      context 'when rating' do
+        let(:criteria) { { rating: 8.5...9.2 } }
+        it { is_expected.to all have_attributes(rating: 8.5...9.2) }
+      end
+
+      context 'when actors' do
+        let(:criteria) { { actors: "James Cameron" } }
+        it { is_expected.to all have_attributes(actors: include("James Cameron")) }
+      end
+
+      context 'when all filters' do
+        let(:criteria) { { actors: "James Cameron", rating: 8.5...9.2, duration: /\d{3}/,
+        genre: "Action", date: 1984, country: 'USA', title: /ermi/i, year: 1940..2000, director: "James Cameron"  } }
+        it { is_expected.to all have_attributes(actors: include("James Cameron"), rating: 8.5...9.2, duration: /\d{3}/,
+          genre: "Action", date: 1984, country: 'USA', title: /ermi/i, year: 1940..2000, director: "James Cameron" ) }
+      end
+
     end
-      describe '#map' do
-        subject { movies.map(&:period).uniq }
-        it { is_expected.to match_array([:ancient, :classic, :modern, :new]) }
+
+    describe '#genre_exist?' do
+
+      subject { collection.genre_exist?(criteria) }
+
+      context "when genre exist" do
+        let(:criteria) { 'Action' }
+        it { is_expected.to be_truthy }
       end
 
-      describe '#select' do
-        subject { movies.select { |m| m.title == 'The Terminator' } }
-        it { is_expected.to have_attributes(count: 1) }
-        its('first.title') { is_expected.to eq('The Terminator') }
+      context "when genre not exist" do
+        let(:criteria) { 'Not exist genre' }
+        it { is_expected.to be_falsey }
       end
+
     end
-  end
+
 end
