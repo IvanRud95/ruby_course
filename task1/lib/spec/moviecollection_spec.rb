3a3a6ee73cfc8e0b9cf4ac847064465419c3e095
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2019-02-11 13:30:37 +0300

    Update moviecollection_spec.rb

--- task1/lib/spec/moviecollection_spec.rb
+++ task1/lib/spec/moviecollection_spec.rb
@@ -16,14 +16,14 @@ describe :MovieCollection do
 
       context 'when filed not exist' do
         let(:criteria) { :director1 }
-        it { expect { subject }.to raise_error(Imbd::MovieCollection::ParametrNotExist) }
+        it { expect { subject }.to raise_error(:MovieCollection::ParametrNotExist) }
       end
 
-      Imbd::MovieCollection::KEYS.each do |field|
+      :MovieCollection::KEYS.each do |field|
         context "when #{field}" do
           let(:criteria) { field }
           its(:count) { should eq 250 }
-          it { is_expected.to be_an(Array).and all( be_an(Imbd::Movie) ) }
+          it { is_expected.to be_an(Array).and all( be_an(:Movie) ) }
           it { is_expected.to be_sorted_by(field) }
         end
       end
@@ -36,7 +36,7 @@ describe :MovieCollection do
 
       context 'when filed not exist' do
         let(:criteria) { :director1 }
-        it { expect { subject }.to raise_error(Imbd::MovieCollection::ParametrNotExist) }
+        it { expect { subject }.to raise_error(:MovieCollection::ParametrNotExist) }
       end
 
       shared_examples "stats" do
@@ -44,7 +44,7 @@ describe :MovieCollection do
         its(:values) { are_expected.to all be_a(Fixnum) }
       end
 
-      Imbd::MovieCollection::KEYS.each do |field|
+      :MovieCollection::KEYS.each do |field|
         context "when #{field}" do
           let(:criteria) { field }
           it_should_behave_like 'stats'
@@ -59,7 +59,7 @@ describe :MovieCollection do
 
       context 'when filed not exist' do
         let(:criteria) { { director1: "James Cameron" } }
-        it { expect { subject }.to raise_error(Imbd::MovieCollection::ParametrNotExist) }
+        it { expect { subject }.to raise_error(:MovieCollection::ParametrNotExist) }
       end
 
       context 'when director' do
