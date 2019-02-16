7a87f5b6e45936d5315bfa150cf8c228cc9e7fa6
IvanRud95 <41362022+IvanRud95@users.noreply.github.com>
2019-02-16 14:45:06 +0300

    Delete spec_helper.rb

--- task1/lib/spec/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-require_relative '../movie_collection'
-require_relative '../movie'
-
-RSpec::Matchers.define :be_sorted_by do |expected|
-  match do |actual|
-    actual == actual.sort_by(&expected)
-  end
-end
